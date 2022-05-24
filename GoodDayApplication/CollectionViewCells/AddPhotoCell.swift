db32de22850e1c7cbced03747c601e82fd3162c7
myungsun <audtjs9987@naver.com>
2022-05-24 18:27:43 +0900

    Modify AddPhotoCell property

--- GoodDayApplication/CollectionViewCells/AddPhotoCell.swift
+++ GoodDayApplication/CollectionViewCells/AddPhotoCell.swift
@@ -8,13 +8,20 @@
 import UIKit
 
 class AddPhotoCell: UICollectionViewCell {
+    // Constants
+    let VIEW_CORNER_RADIUS: CGFloat = 15
+    let VIEW_BORDER_WIDTH: CGFloat = 1
+    
     // UIView
     @IBOutlet weak var containerView: UIView!
-    
+
     override func awakeFromNib() {
         super.awakeFromNib()
         
         // UIView
-        containerView.layer.cornerRadius = 15
+        containerView.layer.cornerRadius = VIEW_CORNER_RADIUS
+        containerView.layer.borderColor = UIColor.black.cgColor
+        containerView.layer.borderWidth = VIEW_BORDER_WIDTH
+        containerView.backgroundColor = ColorManager.shared.getWhite()
     }
 }
